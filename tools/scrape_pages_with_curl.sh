5837c6ff9bf39afe4f865ea338b6ddc224e350f2
Michael Currin <michael.currin@gmail.com>
2019-08-27 21:02:13 +0200

    refactor: Improve scrape bash tool

--- tools/scrape_pages_with_curl.sh
+++ tools/scrape_pages_with_curl.sh
@@ -1,38 +1,46 @@
-#!/bin/bash
+#!/bin/bash -e
 # Fetch HTML pages and store locally.
 #
 # A simple script which use curl to save just a few pages specified manually,
 # if the need is just to get minimal data. This script should be run daily.
 
-# Consider where output should go in project. Top level var, not tools or
-# the python package var.
-#
-# Crontab is not reliable because it requires the machine to be awake for
-# network access. But cron.daily will run whenver the machine is awake,
-# but then must receive argument for user (sent to this script) or be set to
-# run for the user. Consider changing active user and changing directory.
-#
+function write_data {
+  if [ $# -ne 2 ]; then
+    printf "Expected 2 arguments, got: %d\n" $#
+    exit 1
+  fi
+  URI=$1
+  OUT_PATH=$2
 
-# News
+  echo $(basename $OUT_PATH)
 
-URI='https://www.news24.com/SouthAfrica/water_crisis'
+  if [ -s $OUT_PATH ]; then
+    echo ' skip existing non-empty file'
+  else
+    if [ -e $OUT_PATH ]; then
+      echo ' overwriting empty file'
+    else
+      echo ' creating file'
+    fi
+    curl $URI >$OUT_PATH
+  fi
+
+  echo
+}
+
+PROJECT_ROOT=$(cd $(dirname $0) && git rev-parse --show-toplevel)
+OUT_DIR=$PROJECT_ROOT/waterCrisis/properties/var/unprocessed_html
+
 TODAY=$(date +'%Y-%m-%d')
 
-OUTPATH="/home/michael/Scripts/water_scrape/html/news24_water_crisis_$TODAY.html"
-echo $OUTPATH
-curl $URI > $OUTPATH
+NEWS_IN='https://www.news24.com/SouthAfrica/water_crisis'
+WC_IN='https://www.property24.com/property-values/western-cape/9'
+CT_IN='https://www.property24.com/property-values/cape-town/western-cape/432'
 
+NEWS_OUT="$OUT_DIR/news24_water_crisis_$TODAY.html"
+WC_OUT="$OUT_DIR/property_24_western_cape_$TODAY.html"
+CT_OUT="$OUT_DIR/property_24_cape_town_$TODAY.html"
 
-# Properties
-
-URI='https://www.property24.com/property-values/western-cape/9'
-OUTPATH="/home/michael/Scripts/water_scrape/html/property_24_western_cape_$TODAY.html"
-echo $OUTPATH
-curl $URI > $OUTPATH
-
-URI='https://www.property24.com/property-values/cape-town/western-cape/432'
-OUTPATH="/home/michael/Scripts/water_scrape/html/property_24_cape_town_$TODAY.html"
-echo $OUTPATH
-curl $URI > $OUTPATH
-
-sudo chown michael:michael /home/michael/Scripts/water_scrape/html/*
+write_data $NEWS_IN $NEWS_OUT
+write_data $WC_IN $WC_OUT
+write_data $CT_IN $CT_OUT
