4e82d05303e792a021ab11e92e12cb091386feb3
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2022-02-27 15:38:45 +0200

    style

--- tools/scrape_pages_with_curl.sh
+++ tools/scrape_pages_with_curl.sh
@@ -1,31 +1,32 @@
 #!/bin/bash -e
 # Fetch HTML pages and store locally.
 #
-# A simple script which use curl to save just a few pages specified manually,
+# A simple script which use `curl` to save just a few pages specified manually,
 # if the need is just to get minimal data. This script should be run daily.
 #
-# It could be more efficient to check status as 200 before writing but
-# then possibly do in python for ease. The idea is a simple bash script.
+# It could be more efficient to check status as 200 before writing but then
+# possibly do in python for ease. The idea is a simple bash script.
 
 write_data() {
   if [ $# -ne 2 ]; then
     printf "Expected 2 arguments, got: %d\n" $#
     exit 1
   fi
-  URI=$1
-  OUT_PATH=$2
 
-  echo $(basename $OUT_PATH)
+  URI="$1"
+  OUT_PATH="$2"
 
-  if [ -s $OUT_PATH ]; then
+  echo $(basename "$OUT_PATH")
+
+  if [ -s "$OUT_PATH" ]; then
     echo ' skip existing non-empty file'
   else
-    if [ -e $OUT_PATH ]; then
+    if [ -e "$OUT_PATH" ]; then
       echo ' overwriting empty file'
     else
       echo ' creating file'
     fi
-    curl $URI >$OUT_PATH
+    curl "$URI" >"$OUT_PATH"
   fi
 
   echo
@@ -44,6 +45,6 @@ NEWS_OUT="$OUT_DIR/news24_water_crisis_$TODAY.html"
 WC_OUT="$OUT_DIR/property_24_western_cape_$TODAY.html"
 CT_OUT="$OUT_DIR/property_24_cape_town_$TODAY.html"
 
-write_data $NEWS_IN $NEWS_OUT
-write_data $WC_IN $WC_OUT
-write_data $CT_IN $CT_OUT
+write_data "$NEWS_IN" "$NEWS_OUT"
+write_data "$WC_IN" "$WC_OUT"
+write_data "$CT_IN" "$CT_OUT"
